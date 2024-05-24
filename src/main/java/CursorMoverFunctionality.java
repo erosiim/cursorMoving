59df85ff91caac7ec9cc22975e6b7f17338842a7
Alonso Escamilla <exkapp@gmail.com>
2024-05-23 19:29:05 -0600

    Added comments for this code

--- src/main/java/CursorMoverFunctionality.java
+++ src/main/java/CursorMoverFunctionality.java
@@ -2,13 +2,16 @@ import java.awt.*;
 import java.util.Random;
 
 public class CursorMoverFunctionality {
+    //Max position of mouse on screen for X axis
     private static final Integer MAX_SCREEN_X = 150;
+    //Max position of mouse on screen for Y axis
     private static final Integer MAX_SCREEN_Y = 150;
     public static void moveCursor(Integer timeInMiliseconds) throws AWTException, InterruptedException {
         Robot robot = new Robot();
         Random random = new Random();
         while (true){
             robot.mouseMove(random.nextInt(MAX_SCREEN_X), random.nextInt(MAX_SCREEN_Y));
+            //This indicates the delay for move the mouse on the screen
             Thread.sleep(timeInMiliseconds);
         }
     }
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,6 +2,7 @@ import java.awt.*;
 
 public class Main {
     public static void main(String[] args) throws InterruptedException, AWTException {
+        //It takes the argument args[0] as the timeInMiliseconds for moving the cursor
         var timeInMiliSecondsForMoveCursor = Integer.parseInt(args[0]);
         CursorMoverFunctionality.moveCursor(timeInMiliSecondsForMoveCursor);
     }
